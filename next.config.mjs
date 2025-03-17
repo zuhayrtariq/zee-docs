73b1548d30674663138dcd2c46c01794016c6804
Zuhayr <zuhayrtariq4@gmail.com>
2025-03-17 11:54:18 +0500

    ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-export const nextConfig = {
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -7,3 +7,4 @@ export const nextConfig = {
     remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
   },
 };
+export default nextConfig;
