542b06feb245cb719d8823573f4660a24a280605
Zuhayr <zuhayrtariq4@gmail.com>
2025-03-25 18:39:05 +0500

    updated logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,20 @@
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = ({ children, className }: HeaderProps) => {
   return (
     <div className={cn("header", className)}>
-      <Link href='/' className="md:flex-1">
-        <Image 
-          src="/assets/icons/logo.svg"
-          alt="Logo with name"
+      <Link href="/" className="md:flex-1">
+        <Image
+          src="/assets/icons/logo.png"
+          alt="Logo"
           width={120}
           height={32}
           className="hidden md:block"
         />
-        <Image 
-          src="/assets/icons/logo-icon.svg"
+        <Image
+          src="/assets/icons/logo.png"
           alt="Logo"
           width={32}
           height={32}
@@ -23,7 +23,7 @@ const Header = ({ children, className }: HeaderProps) => {
       </Link>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
